1367c750c355a99ae387ac2c2b2775b677bcbe07
andforce <86118@163.com>
2016-01-07 22:46:15 +0800

    update

--- CCF/CCFThreadDetailTableViewController.m
+++ CCF/CCFThreadDetailTableViewController.m
@@ -64,7 +64,7 @@
     
     NSString * message = @":blush;\n[RIGHT][URL=\"https://bbs.et8.net/bbs/showthread.php?p=16695603\"]For Test:Quick Reply[/URL][/RIGHT]";
     
-    [browser reply:entry.urlId :message];
+//    [browser reply:entry.urlId :message];
 }
 
 -(void) browserThreadPosts:(int)page{

c810be3f59dbf3b9be926149eb89b28b28ea3dde
andforce <86118@163.com>
2016-03-27 09:59:42 +0800

    cache avatar

--- CCF/BaseCCFTableViewCell.m
+++ CCF/BaseCCFTableViewCell.m
@@ -13,27 +13,34 @@
     
     CCFCoreDataManager *coreDateManager;
     CCFApi * ccfapi;
+    
+    NSMutableDictionary * avatarCache;
 }
 
 -(instancetype)init{
     if (self = [super init]) {
-        defaultAvatar = [UIImage imageNamed:@"logo.jpg"];
-        
-        ccfapi = [[CCFApi alloc] init];
-        coreDateManager = [[CCFCoreDataManager alloc] initWithCCFCoreDataEntry:CCFCoreDataEntryUser];
+        [self initData];
     }
     return self;
 }
 
 -(instancetype)initWithCoder:(NSCoder *)aDecoder{
     if (self = [super initWithCoder:aDecoder]) {
-        defaultAvatar = [UIImage imageNamed:@"logo.jpg"];
-        
-        ccfapi = [[CCFApi alloc] init];
-        coreDateManager = [[CCFCoreDataManager alloc] initWithCCFCoreDataEntry:CCFCoreDataEntryUser];
+        [self initData];
     }
     return self;
 }
+
+-(void) initData{
+    defaultAvatar = [UIImage imageNamed:@"logo.jpg"];
+    
+    ccfapi = [[CCFApi alloc] init];
+    coreDateManager = [[CCFCoreDataManager alloc] initWithCCFCoreDataEntry:CCFCoreDataEntryUser];
+    avatarCache = [NSMutableDictionary dictionary];
+}
+
+
+
 
 -(void)setData:(id)data{
     [self setData:data];
@@ -41,34 +48,53 @@
 
 -(void)showAvatar:(UIImageView *)avatarImageView userId:(NSString*)userId{
     
-    NSMutableArray * users = [[coreDateManager selectData:^NSPredicate *{
-        return [NSPredicate predicateWithFormat:@"userID = %@", userId];
-    }] copy];
+    NSString * avatarInArray = [avatarCache objectForKey:userId];
     
-    if (users == nil || users.count == 0) {
+    if (avatarInArray == nil) {
+        NSMutableArray * users = [[coreDateManager selectData:^NSPredicate *{
+            return [NSPredicate predicateWithFormat:@"userID = %@", userId];
+        }] copy];
         
-        [ccfapi getAvatarWithUserId:userId handler:^(BOOL isSuccess, NSString * avatar) {
-            [coreDateManager insertOneData:^(id src) {
+        if (users == nil || users.count == 0) {
+            
+            [ccfapi getAvatarWithUserId:userId handler:^(BOOL isSuccess, NSString * avatar) {
+                [coreDateManager insertOneData:^(id src) {
+                    
+                    CCFUserEntry * user =(CCFUserEntry *)src;
+                    
+                    user.userID = userId;
+                    user.userAvatar = avatar;
+                }];
                 
-                CCFUserEntry * user =(CCFUserEntry *)src;
+                [avatarImageView setImageWithURL:[CCFUrlBuilder buildAvatarURL:avatar]];
                 
-                user.userID = userId;
-                user.userAvatar = avatar;
+                [avatarCache setObject:avatar == nil ? @"defaultAvatar": avatar forKey:userId];
             }];
             
-            [avatarImageView setImageWithURL:[CCFUrlBuilder buildAvatarURL:avatar]];
-        }];
-        
+        } else{
+            
+            CCFUserEntry * user = users.firstObject;
+            if (user.userAvatar == nil) {
+                [avatarImageView setImage:defaultAvatar];
+                
+                [avatarCache setValue:@"defaultAvatar" forKey:user.userID];
+                
+            } else{
+                NSURL * url = [CCFUrlBuilder buildAvatarURL:user.userAvatar];
+                [avatarImageView setImageWithURL:url];
+                
+                [avatarCache setValue:user.userAvatar forKey:user.userID];
+            }
+            
+            
+        }
     } else{
-        
-        CCFUserEntry * user = users.firstObject;
-        if (user.userAvatar == nil) {
+        if ([avatarInArray isEqualToString:@"defaultAvatar"]) {
             [avatarImageView setImage:defaultAvatar];
         } else{
-            NSURL * url = [CCFUrlBuilder buildAvatarURL:user.userAvatar];
+            NSURL * url = [CCFUrlBuilder buildAvatarURL:avatarInArray];
             [avatarImageView setImageWithURL:url];
         }
-        
     }
 }
 @end

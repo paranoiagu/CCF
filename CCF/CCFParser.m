184e4f86db1cdd023aef29a0aa0c2a100df8f73c
andforce <86118@163.com>
2016-04-04 15:53:10 +0800

    修改引用的样式

--- CCF/CCFParser.m
+++ CCF/CCFParser.m
@@ -135,6 +135,9 @@
 
 
 -(CCFShowThreadPage *)parseShowThreadWithHtml:(NSString *)html{
+
+    // 修改引用帖子的样式
+    html = [html stringByReplacingOccurrencesOfString:@"<div class=\"smallfont\" style=\"margin-bottom:2px\">引用:</div>" withString:@""];
     
     // 查找设置了字体的回帖
     NSArray * fontSetString = [html arrayWithRegulat:@"<font size=\"\\d+\">"];
@@ -664,6 +667,9 @@
 
 
 -(CCFShowPM *)parsePrivateMessageContent:(NSString *)html{
+    // 修改引用帖子的样式
+    html = [html stringByReplacingOccurrencesOfString:@"<div class=\"smallfont\" style=\"margin-bottom:2px\">引用:</div>" withString:@"<div class=\"smallfont\" style=\"margin-bottom:2px\"> </div>"];
+    
     IGHTMLDocument *document = [[IGHTMLDocument alloc]initWithHTMLString:html error:nil];
     
     // message content
